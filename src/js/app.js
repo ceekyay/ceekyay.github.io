437fb0927471858b5389c0b171e2099c0088b90e
ceekyay <chaitanya-kovuri@uiowa.edu>
2018-09-21 18:41:25 -0400

    Beta-Functional Site #1

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 50,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#FDD57B"
       },
       "shape": {
         "type": "polygon",
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#FDD57B",
         "opacity": 0.4,
         "width": 1
       },
